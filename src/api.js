eb22ac51ed0ff4786389810e3a82e69fe7f3038d
MadukaJustice <jonasjustice98@gmail.com>
2023-09-11 02:12:24 +0100

    Completed weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '018e95aaa0msha6a27a8df725e73p1886f2jsn8da8a7ec1b32',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0e831d11683b2025aadc42915189096a";
